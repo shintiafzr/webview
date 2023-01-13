81dd91b97d048be27c9ca831f4437318b9b0cdf1
shintiafzr <nurulshintia17@gmail.com>
2023-01-13 17:14:37 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:webview/homepage.dart';
+import 'package:webview/webview.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
